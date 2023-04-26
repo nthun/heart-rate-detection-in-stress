7077dd7627c46b3d5bf8d06d0079837e7e6b1bd7
nthun <thomassocska@yahoo.com>
2023-04-26 16:27:54 +0200

    Add subjective table (into a separate file for now) - move to the main file later

--- R/extra_table.R
+++ R/extra_table.R
@@ -1,8 +1,20 @@
 # Create missing descriptive table
 # STAI, várt és észlelt HR, várt stressz - erre kell egy táblázat, meg egy másik korrelációs táblázat
+library(tidyverse)
 library(gt)
+library(correlation)
 
-subj <-
+metric_order <- c("STAI-S Before", "STAI-S After", "Expected stress", "Perceived stress", "Expected HR", "Perceived HR")
+
+stai_sum_wide <-
+  experiment %>% 
+  select(id, 
+         `STAI-S Before_N-back` = stai_s_mental_t0_sum,  
+         `STAI-S After_N-back` = stai_s_mental_t1_sum, 
+         `STAI-S Before_Handgrip` = stai_s_physical_t0_sum, 
+         `STAI-S After_Handgrip` = stai_s_physical_t1_sum)
+
+subj_wide <-
   experiment %>% 
   select(id, 
          `Expected stress_change_N-back` = mental_expected_stress,
@@ -13,44 +25,56 @@ subj <-
          `Expected HR_change_Handgrip` = physical_expected_hr_change,
          `Perceived HR_change_N-back` = mental_experienced_hr_change,
          `Perceived HR_change_Handgrip` = physical_experienced_hr_change) %>% 
+  left_join(stai_sum_wide, by = "id")
+
+subj_long <-
+  subj_wide %>% 
   pivot_longer(cols = -c("id"),
                names_to = c("metric", NA,"task"),
                names_pattern = "^(.*)_(.*)_(.*)",
                values_to = "value")
 
-stai_df <-
-  stai_sum %>% 
-  transmute(task,
-            metric = "STAI-S",
-            measurement = if_else(time == "t0", "Before", "After"),
-            Mean = stai_avg,
-            SD = stai_sd) %>% 
-  unite(metric, c("metric", "measurement"), sep = " ") %>% 
-  pivot_wider(names_from = "task", values_from = c("Mean", "SD"))
+  # Third, we hypothesized that expected stress, physiological reaction, and cardioceptive accuracy would predict perceived stress
 
-subj %>% 
+# Correlations of subj metrics
+
+handgrip_cor <-
+  subj_wide %>% 
+  select(ends_with("_Handgrip")) %>%
+  rename_with(~str_remove(., "_change")) %>% 
+  correlation(redundant = TRUE) %>% 
+  summary(redundant = TRUE) %>% 
+  as_tibble() %>% 
+  separate(Parameter, into = c("metric", "task"), sep = "_") %>% 
+  rename_with(~str_remove(., "_Handgrip"))
+
+nback_cor <-
+  subj_wide %>% 
+  select(ends_with("_N-Back")) %>%
+  rename_with(~str_remove(., "_change")) %>% 
+  correlation(redundant = TRUE) %>% 
+  summary(redundant = TRUE) %>% 
+  as_tibble() %>% 
+  separate(Parameter, into = c("metric", "task"), sep = "_") %>% 
+  rename_with(~str_remove(., "_N-back"))
+
+all_cor <-
+  bind_rows(handgrip_cor, nback_cor)
+
+subj_long %>% 
   group_by(task, metric) %>% 
   summarise(Mean = mean(value, na.rm = TRUE),
             SD = sd(value, na.rm = TRUE), .groups = "drop") %>% 
-  pivot_wider(names_from = "task", values_from = c("Mean", "SD")) %>% 
-  bind_rows(stai_df, .) %>% 
+  bind_rows(stai_df) %>% 
+  left_join(all_cor, by = c("task", "metric")) %>% 
+  mutate(metric = fct_relevel(metric, !!!metric_order)) %>% 
+  arrange(task, metric) %>% 
+  select(task, metric, Mean, SD, all_of(metric_order)) %>% 
   gt() %>% 
-  fmt_number(c(Mean_Handgrip, `Mean_N-back`, SD_Handgrip, `SD_N-back`), decimals = 1) %>%
-  cols_merge(columns = c("Mean_Handgrip", "SD_Handgrip"), pattern = "{1} ({2})") %>%
-  cols_merge(columns = c("Mean_N-back", "SD_N-back"), pattern = "{1} ({2})") %>%
-  cols_label("metric" = "",
-             "Mean_Handgrip" = "Handgrip	",
-             "Mean_N-back" = "N-back")
-
-
-
-
-# Third, we hypothesized that expected stress, physiological reaction, and cardioceptive accuracy would predict perceived stress
-
-
-
-
-
-
-
+  fmt_number(c("Mean", "SD"), decimals = 1) %>% 
+  fmt_number(`STAI-S Before`:`Perceived HR`, decimals = 2)
   
+
+
+
+
